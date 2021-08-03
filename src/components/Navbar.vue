943ab85103eced489193803b9f3fa13acefd91b7
Dilmohan <dilmohan11@gmail.com>
2021-08-03 15:54:22 +0530

    Navbar changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-      <h1 class="title text-5xl text-center m-5 capitalize py-5">Tell me a Joke or show me a Meme</h1>
+      <h1 class="title text-5xl text-center m-5 capitalize py-4">Tell me a Joke or show me a Meme</h1>
       <!-- <nav class="text-center text-xl font-semibold text-blue-800 mb-5">
           <router-link :to="{name: 'Jokes'}" class="pr-5">Jokes</router-link>
           <router-link :to="{name: 'Memes'}">Memes</router-link>
